e9a3f7dd931b6909bf656ccd2a84a19a55de4be3
berur4 <berurcaleb@gmail.com>
2024-11-05 11:58:17 +0300

    array.finf lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function superbowlWin(record) {
+    function isSuperbowlWin(game) {
+      return game.result === "W"; // Corrected to uppercase "W"
+    }
+  
+    const winRecord = record.find(isSuperbowlWin);
+    return winRecord ? winRecord.year : undefined;
+  }
+  
+  const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+  ];
+  
+  console.log(superbowlWin(record));
